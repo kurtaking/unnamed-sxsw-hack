b427e309868c3fbd25bace527b224a48d73c137e
Bryan Deloeste <bryandeloeste@gmail.com>
2016-03-15 21:16:22 -0500

    helloooo

--- musicstreaming/models.py
+++ musicstreaming/models.py
@@ -2,23 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
-	user_id = models.AutoField(primary_key=True)
-	user_name = models.CharField(max_length=512)
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=512)
 
-	def __unicode__(self):
-		return self.user_id
+    def __unicode__(self):
+        return self.user_id
+
 
 class Music(models.Model):
-	music_id = models.AutoField(primary_key=True)
-	pathname = models.CharField(max_length=512)
-	users = models.ManyToManyField(User, through='Forkship')
-	
-	def __unicode__(self):
-		return self.pathname
-	
+    music_id = models.AutoField(primary_key=True)
+    pathname = models.CharField(max_length=512)
+    users = models.ManyToManyField(User, through='Forkship')
+
+    def __unicode__(self):
+        return self.pathname
+
+
 class Forkship(models.Model):
-	user = models.ForeignKey(User)
-	music = models.ForeignKey(Music)
+    user = models.ForeignKey(User)
+    music = models.ForeignKey(Music)
