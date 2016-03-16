55478583b505cff3c05cad45f913e1f534083c80
Kurt King <kurtaking@gmail.com>
2016-03-16 13:05:29 -0500

    Test

--- musicstreaming/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^dashboard', views.dashboard, name='dashboard'),
-    url(r'^user_feed', views.user_feed, name='user_feed'),
-    url(r'^api/upload', views.upload, name='upload')
-]
